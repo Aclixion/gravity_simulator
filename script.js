c6321d5613d0e67c42ffc1673984cbfab3b90ed5
David Lei <DavidtheDefender2003@gmail.com>
2022-12-24 17:20:37 -0800

    Remove period on comment

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 const canvas = document.getElementById("canvas"); // Canvas element
-const ctx = canvas.getContext("2d"); // Context for drawing shapes
+const ctx = canvas.getContext("2d"); // Context for drawing shape
