730d9cbba5571e36d3227faaaef620bc5ea310ec
David Lei <DavidtheDefender2003@gmail.com>
2022-12-26 12:56:18 -0800

    Add option input variables

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const canvas = document.getElementById("canvas"); // Canvas element
 const ctx = canvas.getContext("2d"); // Context for drawing shape
+
+const massInput = doocument.querySelector(".mass-input"); // Input for mass
+const radiusInput = doocument.querySelector(".radius-input"); // Input for radius
+const colorInput = doocument.querySelector(".color-input"); // Input for color
