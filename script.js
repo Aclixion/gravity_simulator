369d78dd03fa675ec4f413aacc0d0a74960612c2
David Lei <DavidtheDefender2003@gmail.com>
2022-12-26 20:20:01 -0800

    Add comment and reposition code

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ const massInput = document.querySelector(".mass-input"); // Input for mass
 const radiusInput = document.querySelector(".radius-input"); // Input for radius
 const colorInput = document.querySelector(".color-input"); // Input for color
 
-canvas.addEventListener("click", addPlanet);
-
-function addPlanet(e) {
+// Create a planet at the location the user clicks on the canvas
+function createPlanet(e) {
     ctx.beginPath();
     ctx.arc(e.offsetX, e.offsetY, radiusInput.value, 0, 2 * Math.PI);
     ctx.stroke();
 }
+
+canvas.addEventListener("click", createPlanet);
