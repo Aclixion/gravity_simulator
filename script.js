572ec1da74fad53bb41e49d8dcb00d9663d12023
David Lei <DavidtheDefender2003@gmail.com>
2022-12-26 20:19:26 -0800

    Add create planet functionn

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const ctx = canvas.getContext("2d"); // Context for drawing shape
 const massInput = document.querySelector(".mass-input"); // Input for mass
 const radiusInput = document.querySelector(".radius-input"); // Input for radius
 const colorInput = document.querySelector(".color-input"); // Input for color
+
+canvas.addEventListener("click", addPlanet);
+
+function addPlanet(e) {
+    ctx.beginPath();
+    ctx.arc(e.offsetX, e.offsetY, radiusInput.value, 0, 2 * Math.PI);
+    ctx.stroke();
+}
