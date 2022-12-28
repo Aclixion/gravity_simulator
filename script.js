15c0e38f10a6966f32333fe92607251eff8868b1
David Lei <DavidtheDefender2003@gmail.com>
2022-12-27 20:44:05 -0800

    Add planet constructor

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ const ctx = canvas.getContext("2d"); // Context for drawing shape
 const massInput = document.querySelector(".mass-input"); // Input for mass
 const radiusInput = document.querySelector(".radius-input"); // Input for radius
 const colorInput = document.querySelector(".color-input"); // Input for color
+
+function Planet(mass, radius, color) {
+    this.mass = mass;
+    this.radius = radius;
+    this.color = color;
+    this.position = {
+        x: 0,
+        y: 0,
+        z: 0
+    };
+    this.velocity = {
+        x: 0,
+        y: 0,
+        z: 0
+    }
+    this.acceleration = {
+        x: 0,
+        y: 0,
+        z: 0
+    }
+};
